cdd549a865d0e3d568599400cc355e9ef2d4da22
Miruna <miruna_munteanu2009@yahoo.com>
2022-05-12 20:54:04 +0300

    commit 2

--- src/Turist.java
+++ src/Turist.java
@@ -3,18 +3,10 @@ import java.util.List;
 public class Turist {
 
 	public static void main(String[] args) throws CloneNotSupportedException{
-		String s1 = "Shine bright like a diamond";
-	    String s2 = "I choose to be happy";
-	    String s3 = new String("We're like diamonds in the sky");
-	    String s4 = new String("You're a shooting star I see");
-	    System.out.println(s1);
-	    System.out.println(s2);
-	    System.out.println(s3);
-	    System.out.println(s4);
-		System.out.println("Shine bright like a diamond" + s1);
-		System.out.println("Find light in the beautiful sea" + s2);
-		System.out.println("You and I, you and I" + s3);
-		System.out.println(s4 + "A vision of ecstasy" + "When you hold me, I'm alive");
+		System.out.println("Shine bright like a diamond");
+		System.out.println("Find light in the beautiful sea");
+		System.out.println("You and I, you and I");
+		System.out.println("A vision of ecstasy" + "When you hold me, I'm alive");
 		System.out.println("We're like diamonds in the sky");
 	}
 
